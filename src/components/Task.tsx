a57ea0c08eb314ea3837a5c9aa4b80cd63838a78
RayAra2004 <raynan.araujo@gmail.com>
2023-12-14 17:40:56 -0300

    feat: layout of task

---
+++ src/components/Task.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export default function Task(){
+    return(
+        <SCTask>
+            <td>Tarefa A</td>
+            <td>Pendente</td>
+            <td>
+                <span className="material-symbols-outlined">image</span>
+            </td>
+            <td className="pencil-icon">
+                <span className="material-symbols-outlined">edit</span>
+            </td>
+            <td>
+                <button>Concluir</button>
+            </td>
+        </SCTask>
+    );
+}
+
+const SCTask = styled.tr`
+
+
+    .pencil-icon{
+        text-align: right;
+    }
+    
+    :last-child{
+        width: 15%;
+
+        button{
+            width: 100%;
+            height: 30px;
+        }
+    }
+
+`
